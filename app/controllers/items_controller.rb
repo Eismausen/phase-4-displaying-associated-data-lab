31faaf6d6312ea5108df1af3550d57cfd1b2ac18
David Avery <eismausen@gmail.com>
2021-12-18 19:37:09 -0800

    commit tree 9bd735f93bc625918a3b67caf8791e67354fa947

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,7 @@
 class ItemsController < ApplicationController
+
+    def index
+        all_items = Item.all
+        render json: all_items, include: :user
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+
+    def show
+        this_user = User.find(params[:id])
+        render json: this_user, include: :items
+    end
 end
